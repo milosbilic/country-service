170cfdd6118c018506526c95fb24142b49c98c92
milosbilic <milosrcz@gmail.com>
2018-12-21 15:29:13 +0100

    using rest template to call another microservice

--- src/main/java/com/example/countryservice/controller/CountryController.java
+++ src/main/java/com/example/countryservice/controller/CountryController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.countryservice.client.CityClient;
+import com.example.countryservice.dto.CityDto;
 import com.example.countryservice.dto.CountryDto;
 import com.example.countryservice.dto.converter.CountryToCountryDto;
 import com.example.countryservice.model.Country;
@@ -25,6 +27,8 @@ public class CountryController {
 	@Autowired
 	private CountryToCountryDto toCountryDto;
 	
+	private RestTemplate restTemplate = new RestTemplate();
+	
 	@GetMapping
 	public List<Country> listAll() {
 		return repository.findAll();
@@ -33,7 +37,11 @@ public class CountryController {
 	@GetMapping("/{id}/with-cities")
 	public CountryDto findByIdWithCities(@PathVariable Long id) {
 		CountryDto retVal = toCountryDto.convert(repository.findById(id).get());
-		retVal.setCities(cityClient.findByCountry(id));
+//		retVal.setCities(cityClient.findByCountry(id));
+		
+		List<CityDto> cities = (List<CityDto>) restTemplate.getForEntity(
+				"https://city-service.herokuapp.com/country/" + id, List.class);
+		retVal.setCities(cities);
 		return retVal;
 	}
 	
