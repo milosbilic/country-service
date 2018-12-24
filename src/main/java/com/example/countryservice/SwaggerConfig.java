0a5b60f3a7456798caadcd68b1cfebbc98b694ad
milosbilic <milosrcz@gmail.com>
2018-12-24 14:43:37 +0100

    fixed the package name

--- src/main/java/com/example/countryservice/SwaggerConfig.java
+++ src/main/java/com/example/countryservice/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+                .select().apis(RequestHandlerSelectors.basePackage("com.example.countryservice.controller"))
                 .paths(PathSelectors.any())
                 .build();
              
