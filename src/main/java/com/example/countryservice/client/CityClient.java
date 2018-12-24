a63f65278c68f36aa05f43a7c6e16c90665c550a
milosbilic <milosrcz@gmail.com>
2018-12-24 11:06:12 +0100

    added enable swagger annotation

--- src/main/java/com/example/countryservice/client/CityClient.java
+++ src/main/java/com/example/countryservice/client/CityClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.countryservice.dto.CityDto;
 
-@FeignClient(name = "city-service", configuration = FeignClientConfig.class)
+@FeignClient(name = "city-service")
 public interface CityClient {
 
 	@GetMapping("/country/{countryId}")
